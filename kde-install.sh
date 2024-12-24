6261954e776e2c20ddec84cac7bac21931337497
Mathias Wouters <67171445+mathiaswouters404@users.noreply.github.com>
2024-12-24 11:54:52 +0100

    Update kde-install.sh

--- kde-install.sh
+++ kde-install.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
 # Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
 
 # Update packages list and update system
 apt update
 apt upgrade -y
+
+# Making .config and Moving config files and background to Pictures
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+mkdir -p /home/$username/Pictures
+mkdir -p /home/$username/Pictures/wallpapers
+cp -R dotconfig/* /home/$username/.config/
+cp -R wallpapers/* /home/$username/Pictures/wallpapers/
+mv user-dirs.dirs /home/$username/.config
+chown -R $username:$username /home/$username
+
+# Installing Essential Programs 
+
+# Installing Other less important Programs
