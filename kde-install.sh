407abb5e88bc445ed7e976c2cee71b6c621ebfa0
Mathias Wouters <67171445+mathiaswouters404@users.noreply.github.com>
2024-12-24 14:31:32 +0100

    Update kde-install.sh

--- kde-install.sh
+++ kde-install.sh
@@ -141,6 +141,13 @@ ufw allow http
 ufw allow https
 ufw enable
 
+# Setup bootloader
+git clone https://github.com/mathiaswouters404/bootloader-theme
+cd bootloader-theme
+sudo ./install.sh
+cd ../debian-mathias
+rm -rf bootloader-theme
+
 # End
 echo "END"
 
